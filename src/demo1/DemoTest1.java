48768b4e9a28c120f6c1ef4c79f8e1d0ed33d54d
carol <carol@10.0.0.241>
2025-01-29 21:11:45 -0500

    Methods

--- src/demo1/DemoTest1.java
+++ src/demo1/DemoTest1.java
@@ -4,6 +4,38 @@ public class DemoTest1 {
 	
 	public static void main (String args[]) {
 		System.out.println("Java");
+		
+		String brand = "Puma";
+		String slogan = "";
+		boolean isSloganFound = true;
+		
+		switch (brand) {
+		case "Nike":
+		slogan = "Just Do It";
+		break;
+		case "Adidas":
+		slogan = "Impossible is Nothing";	
+		break;
+		case "Puma":
+		slogan = "Forever Faster";	
+		break;
+		case "Reebok":
+		slogan = "Not Found";
+		break;
+		default:
+			 slogan = "Not Found";
+			 isSloganFound = false;
+	
+		
+		
+		}
+		if (isSloganFound == true) {
+			System.out.println (brand + "'s slogan is " + slogan);
+		}else {
+			System.out.println(brand +"'s slogan " + slogan +" . This message suppose to give me hard time ");
+		}
+		
+		
+		
 	}
-
 }
